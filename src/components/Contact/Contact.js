600f9509adc73fb67c8db6de3dbce2fe7981016a
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-05 20:45:37 +0000

    Added basic template to follow

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,14 +3,14 @@ import React from "react";
 function Contact() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Get in Touch</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Include form to send email directly
       </p>
+      <h2>Feel free to connect with me</h2>
+      <div>
+        Links to github, linkedin, cv
+      </div>
     </div>
   );
 }
