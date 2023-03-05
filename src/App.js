f94563e52668d7883fd02a7761ddde36c3a4129e
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-05 19:40:49 +0000

    Edited nav bar

--- src/App.js
+++ src/App.js
@@ -2,24 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Home from './components/pages/Home';
-import About from './components/pages/About';
-import Blog from './components/pages/Blog';
 import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
       <div>
         <NavTabs />
-        {/* Wrap Route elements in a Routes component */}
         <Routes basename="react-portfolio">
-          {/* Define routes using the Route component to render different page components at different paths */}
-          {/* Define a default route that will render the Home component */}
           <Route path="/react-portfolio" element={<Home />} />
-          <Route path="/react-portfolio/about" element={<About />} />
-          <Route path="/react-portfolio/blog" element={<Blog />} />
-          {/* Define a route that will have descendant routes */}
-          <Route path="/react-portfolio/contact/*" element={<Contact />} />
+          <Route path="/react-portfolio/projects" element={<Projects />} />
+          <Route path="/react-portfolio/contact" element={<Contact />} />
         </Routes>
       </div>
     </Router>
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,60 +3,49 @@ import { NavLink } from 'react-router-dom';
 
 function NavTabs() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <NavLink
-          to="/react-portfolio"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/react-portfolio/about"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/react-portfolio/blog"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Blog
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/react-portfolio/contact"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Contact
-        </NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink
-          to="/react-portfolio/contact/learn"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Learn
-        </NavLink>
-      </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse">
+          <ul className="navbar-nav nav-tabs ms-auto">
+            <li className="nav-item">
+              <NavLink
+                to="/react-portfolio"
+                end
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/react-portfolio/projects"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/react-portfolio/contact"
+                className={({ isActive }) =>
+                  isActive ? 'nav-link active' : 'nav-link'
+                }
+              >
+                Contact Me
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
