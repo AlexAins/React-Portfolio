7deefdd91fcad40dc83590bf7e43aafb86793c73
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-08 16:45:50 +0000

    Edited nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 
 function NavTabs() {
   return (
-    <ul className="navbar-nav ms-auto">
+    <ul className="navbar-nav ms-auto me-3">
       <li className="nav-item">
-        <Link to="/">
+        <Link to="/" className="text-decoration-none mx-3">
           Home
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/projects">
+        <Link to="/projects" className="text-decoration-none mx-3">
           Projects
         </Link>
       </li>
       <li className="nav-item">
-        <Link to="/contact">
+        <Link to="/contact" className="text-decoration-none mx-3">
           Contact Me
         </Link>
       </li>
