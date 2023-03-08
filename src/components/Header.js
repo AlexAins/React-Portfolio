e0a03bc06a22604089bbbc719db3a0a03443f5c1
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-08 19:47:36 +0000

    edited logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import LogoBlue from '../assets/logoBlue.png'
 
 function Header(){
     return(
-    <nav className="navbar navbar-expand-md bg-secondary py-3">
+    <nav className="navbar navbar-expand-md bg-secondary">
         <div className="container">
-            <img className="navbar-brand img-fluid" src={LogoBlue} alt="Logo of a blue letter A"/>
+            <img className="navbar-brand img-fluid" src={LogoBlue} alt="Logo of a blue letter A" style={{height: '60px'}}/>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
