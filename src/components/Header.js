9a5c5d61450fd68103194ca039541e71391c131b
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-08 17:28:56 +0000

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavTabs from './NavTabs';
+import LogoBlue from '../assets/logoBlue.png'
 
 function Header(){
     return(
     <nav className="navbar navbar-expand-md bg-secondary py-3">
         <div className="container">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <img className="navbar-brand img-fluid" src={LogoBlue} alt="Logo of a blue letter A"/>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
