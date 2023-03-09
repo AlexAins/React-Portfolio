205392bbe69b8b54396abe21a391782e2e8c1fb4
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-09 17:51:50 +0000

    Edited footer style and added logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,17 +2,22 @@ import React from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 import { CgFileDocument } from "react-icons/cg";
-import CV from "../../assets/AlexanderAinslieCV.pdf"
+import CV from "../../assets/AlexanderAinslieCV.pdf";
+import logoBlack from "../../assets/logoBlack.png";
+import "./Footer.css";
 
 function Footer(){
     return(
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-6 d-flex align-items-center">
-                    <h4 className="fst-italic">Developed and Designed by Alexander Ainslie</h4>
+                <div className="col-md-4 d-flex align-items-center">
+                    <h4 className="footer-title">Developed and Designed by Alexander Ainslie</h4>
                 </div>
-                <div className="col-md-3 offset-md-3">
-                    <ul className="footerLinks nav list-unstyled d-flex justify-content-center">
+                <div className="col-md-4 d-flex align-items-center justify-content-center">
+                    <img src={logoBlack} alt="Logo in black and white" style={{height: '2px'}}/>
+                </div>
+                <div className="col-md-4">
+                    <ul className="footerLinks nav list-unstyled d-flex justify-content-end">
                         <li className="footerIcons mx-3">
                             <a
                                 href="https://github.com/AlexAins"
