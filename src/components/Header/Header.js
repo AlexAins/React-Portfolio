d407d30d6faf4e4988b422de29a394a139e37205
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-10 17:23:26 +0000

    Added css to underline navbar on hover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavTabs from './NavTabs';
 import LogoBlue from '../../assets/logoBlue.png'
+import "./Header.css"
 
 function Header(){
     return(
     <nav className="navbar navbar-expand-md bg-secondary">
         <div className="container">
-            <img className="navbar-brand img-fluid" src={LogoBlue} alt="Logo of a blue letter A" style={{height: '60px'}}/>
+            <img className="navbar-brand img-fluid" src={LogoBlue} alt="Logo of a blue letter A" style={{height: '50px'}}/>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- src/components/Header/NavTabs.js
+++ src/components/Header/NavTabs.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Header.css"
 
 function NavTabs() {
   return (
     <ul className="navbar-nav ms-auto me-3">
-      <li className="nav-item">
-        <Link to="/" className="text-decoration-none mx-3">
+      <li className="nav-items mx-5">
+        <Link to="/" className="text-decoration-none nav-item">
           Home
         </Link>
       </li>
-      <li className="nav-item">
-        <Link to="/projects" className="text-decoration-none mx-3">
+      <li className="nav-items mx-5">
+        <Link to="/projects" className="text-decoration-none nav-item">
           Projects
         </Link>
       </li>
-      <li className="nav-item">
-        <Link to="/contact" className="text-decoration-none mx-3">
+      <li className="nav-items mx-5">
+        <Link to="/contact" className="text-decoration-none nav-item">
           Contact Me
         </Link>
       </li>
